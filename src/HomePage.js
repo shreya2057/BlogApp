7c47cfa5c6dcf3f875830bf0b1c547fa6041b376
shreya2057 <shreya2057@gmail.com>
2022-01-26 19:10:34 +0545

    NavBar and homepage created

---
+++ src/HomePage.js
@@ -0,0 +1,10 @@
+const HomePage = () => {
+    const heading = "Welcome to blog page"
+    return ( 
+        <div className="home">
+            <h1>{ heading }</h1>
+        </div>
+     );
+}
+ 
+export default HomePage;
